c6b8c5121cba6d81afe916e71cbfcd4a570099c3
Jethro Pham <vinhngocphamhp@gmail.com>
2018-11-30 02:23:41 +0700

    Delete fmSize.js

--- fmSize.js
+++
@@ -1,9 +0,0 @@
-app.filter('fomatSize', function() {
-	return function(size, fmSize) {
-		if(size) {
-			return _.floor(_.divide(_.divide(size, 1024), 1024), 2).toString() + fmSize;
-		}else {
-			return "NA";
-		}
-	}
-});
